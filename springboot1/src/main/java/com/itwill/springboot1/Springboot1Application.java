024ac95f934beb4455737050d898dd29ac1176b4
ChoiMari <chl98639863@gmail.com>
2024-07-19 12:12:56 +0900

    20240719 쿼리스트링/path variable

--- springboot1/src/main/java/com/itwill/springboot1/Springboot1Application.java
+++ springboot1/src/main/java/com/itwill/springboot1/Springboot1Application.java
@@ -3,7 +3,7 @@ package com.itwill.springboot1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //-> 굉장히 중요하다고 함
 public class Springboot1Application {
 
 	public static void main(String[] args) {
--- springboot1/src/main/java/com/itwill/springboot1/controller/HomeController.java
+++ springboot1/src/main/java/com/itwill/springboot1/controller/HomeController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.springboot1.dto.Author;
 import com.itwill.springboot1.dto.Book;
@@ -50,7 +52,7 @@ public class HomeController {
 	
 	@GetMapping("/book/list")
 	public void bookList(Model model) {
-		//return 타입이 void인 경우 뷰의 이름은 요청 주소와 같음
+		//return 타입이 void인 경우 뷰의 이름은 요청 주소와 같음. book폴더의 list.html을 찾아감
 		log.info("bookList()");
 		
 		//도서 목록 더미 데이터를 저장하기 위한 리스트.
@@ -67,6 +69,7 @@ public class HomeController {
 			list.add(book);
 		}
 		
+		//북을 1개 더 만듬 author가 없는 북을 1개 추가
 		Book b = Book.builder().id(10).title("-_-").build(); //author => null이됨
 		//리스트에 넣음 -> 6권
 		list.add(b);
@@ -76,5 +79,43 @@ public class HomeController {
 		
 		
 	}
+	
+	@GetMapping("/book/details")
+	public void bookDetails(Model model, @RequestParam(name= "id") int id) { //쿼리스트링 받는 방법  @RequestParam(name= "id") int id
+		//요청 파라미터 id값을 찾고, 해당 id를 갖는 Book 객체를 만들고
+		//모델에 Book 객체를 속성(attr 애트리뷰트)으로 저장. 뷰로 전달
+		
+		log.info("bookDetails(id={})",id);
+		
+		Book book = Book.builder()
+				.id(id)
+				.title("책제목..")
+				.author(Author.builder().firstName("깔깔").lastName("김").build())
+				.build();
+		
+		model.addAttribute("myBook",book);
+	
+	}
+	
+	
+	//path variable을 포함하는 요청을 처리하는 메서드
+	@GetMapping("/book/details/{id}") //-> 이거 쓸땐 메서드 이름 void로 하면 안됨. String으로 해야함. 
+	//안그럼 id 값이 변하는 대로 HTML파일이 필요함.
+	public String bookDetailsPathVariable(@PathVariable(name="id") int id, Model model) { 
+		//@GetMapping("/book/details/{변하는값변수}")에 {변하는값변수}있는거와 똑같이 @PathVariable에 (name="변하는값변수") name값 써야함
+		log.info("bookDetailsPathVariable(id={}",id);
+		
+		Book book = Book.builder()
+				.id(id)
+				.title("책 제목..-_-")
+				.author(Author.builder().firstName("저자").lastName("책").build())
+				.build();
+		model.addAttribute("myBook", book);
+		
+		return "book/details";
+		//templates밑에서 찾음. templates는 생략가능
+		//뒤에는 .html 생략 가능
+	}
+	
 	
 }
